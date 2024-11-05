4dfec1e3adc6ed271d9ee44dab610fc387d9b1c3
srSenhor <viniciusfeliciano14@gmail.com>
2024-11-04 23:04:19 -0300

    Initial commit

---
+++ src/main/java/me/dio/trilha_java_trivia_api/Application.java
@@ -0,0 +1,13 @@
+package me.dio.trilha_java_trivia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
